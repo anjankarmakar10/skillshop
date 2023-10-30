b03f577167f9c82d9e66941082e516677991161d
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-10-30 11:25:16 +0600

    Implement DarkMode

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -7,15 +7,19 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
     <NavigationMenu className="sticky top-0 z-10 bg-white dark:bg-slate-950 border-b p-4 max-w-full flex justify-between items-center">
-      <Link href="/docs" className="font-semibold text-lg">
+      <Link href="/docs" className="font-semibold text-lg dark:text-white">
         SkillShop
       </Link>
 
       <NavigationMenuList>
+        <NavigationMenuItem>
+          <ThemeToggle />
+        </NavigationMenuItem>
         <NavigationMenuItem>
           <Link href="/jobs" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
