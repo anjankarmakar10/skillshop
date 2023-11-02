80f09b80595eba31f465b7637d70725d9cb718fb
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-02 16:04:42 +0600

    Implement authentication

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,35 @@
 "use client";
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
+  const { status, data: session } = useSession();
+
   return (
-    <header className="border-b">
+    <header className="border-b ">
       <NavigationMenu className="sticky top-0 z-10 bg-white dark:bg-slate-950  p-4  max-w-[1340px] mx-auto  flex justify-between items-center">
         <Link href="/" className="font-semibold text-lg dark:text-white">
           SkillShop
@@ -28,6 +46,50 @@ const Navbar = () => {
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
+          {status === "unauthenticated" && (
+            <NavigationMenuItem>
+              <Link href="/api/auth/signin" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Sign In
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          )}
+          {status === "authenticated" && (
+            <DropdownMenu>
+              <NavigationMenuItem>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline">{session.user?.name}</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Profile</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link
+                      className="flex items-center"
+                      href="/api/auth/signout"
+                    >
+                      <LogOut className="mr-2 h-4 w-4" />
+                      <span>Log out</span>
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </NavigationMenuItem>
+            </DropdownMenu>
+          )}
+          {status === "loading" && (
+            <NavigationMenuItem>
+              <Skeleton className="h-8 w-[90px] rounded-md" />
+            </NavigationMenuItem>
+          )}
         </NavigationMenuList>
       </NavigationMenu>
     </header>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "./Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import AuthProvider from "./auth/AuthProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,17 +21,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="min-h-screen">
-            <Navbar />
-            <div className="max-w-[1340px] mx-auto p-4">{children}</div>
-          </div>
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-h-screen">
+              <Navbar />
+              <div className="max-w-[1340px] mx-auto p-4">{children}</div>
+            </div>
+          </ThemeProvider>
+        </AuthProvider>
         <Toaster />
       </body>
     </html>
