1f2ecb680b2cc788fa234bc01ab00c05133a58d1
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-04 17:52:40 +0600

    set default router page as job listing page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/jobs",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
